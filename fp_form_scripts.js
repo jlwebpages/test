392a567674aa6b2e7436e8e5e6b0e66957927fee
jlwebpages <jerlaguna@gmail.com>
2025-07-19 19:20:49 -0700

    Test.

--- fp_form_scripts.js
+++ fp_form_scripts.js
@@ -4352,9 +4352,7 @@ function build_regular_season_form()
 
    if (number_of_rs_games < max_number_of_rs_games)
    {
-      d.writeln('<div style="margin: 20px 5px 5px 5px; max-width: '+(window.screen.width-60)+'px">');
-      d.writeln('<font style="font-family: Calibri, sans-serif; font-size: 12pt"><b>Open Date:</b>&nbsp;&nbsp;'+open_date+'</font>');
-      d.writeln('</div>');
+      d.writeln('<div style="font-family: Calibri, sans-serif; font-size: 12pt; text-align: center; margin: 20px 5px 5px 5px; display: block"><b>Open Date:</b>&nbsp;&nbsp;'+open_date+'</div>');
    }
 
    if (mode == "picks")
