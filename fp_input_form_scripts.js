e69b8e92e1a6ff79786fcc82ef625eae0fe51697
jlwebpages <jerlaguna@gmail.com>
2025-08-04 23:29:12 -0700

    Test.

--- fp_input_form_scripts.js
+++ fp_input_form_scripts.js
@@ -421,7 +421,7 @@ function build_regular_season_form()
    d.writeln('');
    d.writeln('      // Format picks for display.');
    d.writeln('');
-   d.writeln('      picks_message  = "\\n<table style=\\"font-family: Calibri, sans-serif; font-size: .9em; margin-right: auto; margin-left: auto\\">";');
+   d.writeln('      picks_message  = "\\n\\n<table style=\\"font-family: Calibri, sans-serif; font-size: .9em; margin-right: auto; margin-left: auto\\">";');
    d.writeln('      picks_message += "\\n<tr><td nowrap style=\\"padding-bottom: 5px\\"><b>Create an e-mail message and send it to:</b></td></tr>"');
    d.writeln('      picks_message += "\\n<tr><td nowrap style=\\"padding-bottom: 5px; padding-left: 10px\\">fp@socal.rr.com</td></tr>"');
    d.writeln('      picks_message += "\\n<tr><td nowrap style=\\"padding-bottom: 5px\\"><b>Copy the following into the subject line:</b></td></tr>"');
@@ -3184,7 +3184,7 @@ function build_postseason_form()
    d.writeln('');
    d.writeln('      // Format picks for display.');
    d.writeln('');
-   d.writeln('      picks_message  = "\\n<table style=\\"font-family: Calibri, sans-serif; font-size: .9em; line-height: 1.5; margin-right: auto; margin-left: auto\\">";');
+   d.writeln('      picks_message  = "\\n\\n<table style=\\"font-family: Calibri, sans-serif; font-size: .9em; line-height: 1.5; margin-right: auto; margin-left: auto\\">";');
    d.writeln('      picks_message += "\\n<tr><td nowrap style=\\"padding-bottom: 5px\\"><b>Create an e-mail message and send it to:</b></td></tr>"');
    d.writeln('      picks_message += "\\n<tr><td nowrap style=\\"padding-bottom: 5px; padding-left: 10px\\">fp@socal.rr.com</td></tr>"');
    d.writeln('      picks_message += "\\n<tr><td nowrap style=\\"padding-bottom: 5px\\"><b>Copy the following into the subject line:</b></td></tr>"');
