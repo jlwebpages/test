869db9f15a16797eeb05a63c27293bce84b10de3
jlwebpages <jerlaguna@gmail.com>
2025-10-07 08:25:00 -0700

    Test.

--- fp_javascript_functions_forms.js
+++ fp_javascript_functions_forms.js
@@ -13,7 +13,7 @@ function adjust_mobile_viewport_height(document,form)
    {
       if ( (window.screen.height > window.screen.width) && (top.document.getElementById("viewport") != null) )
       {
-         var document_height = document.body.scrollHeight;
+         var document_height = document.body.scrollHeight + 5;
 
          if ( (form == "postseason") && (document_height < 600) ) document_height = 600;
 
