6f44dbdc9fad4a894bbeac7f1aa6c4ca186530b5
jlwebpages <jerlaguna@gmail.com>
2026-01-02 12:38:54 -0800

    Test.

--- scripts.js
+++ scripts.js
@@ -170,7 +170,7 @@ function display_image_with_caption(image_file_name,gallery_name,image_number)
 
       html_string += '';
       html_string += '';
-      html_string += '<div id="art_container" class="'+art_container_class+' fade_in" '+art_container_style+'">';
+      html_string += '<center><div id="art_container" class="'+art_container_class+' fade_in" '+art_container_style+'">';
       html_string += '';
       html_string += '   <button class="nav_button nav_left_offset"   style="top: '+nav_button_vertical_position+'"  onclick="navigate_to_next_image(\''+gallery_name+'\',\''+image_number+'\',\''+max_number_of_images+'\',\'left\');"><div class="nav_left_shape"></div></button>';
       html_string += '   <button class="nav_button nav_right_offset"  style="top: '+nav_button_vertical_position+'"  onclick="navigate_to_next_image(\''+gallery_name+'\',\''+image_number+'\',\''+max_number_of_images+'\',\'right\');"><div class="nav_right_shape"></div></button>';
@@ -186,8 +186,7 @@ function display_image_with_caption(image_file_name,gallery_name,image_number)
       html_string += '      </div>';
       html_string += '   </div>';
       html_string += '';
-      html_string += '</div>';
-      html_string += '';
+      html_string += '</div></center>';
       html_string += '';
 
       document.body.innerHTML = html_string;
