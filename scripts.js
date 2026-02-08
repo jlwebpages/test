e54e6acccca2f94a72d3916e3c25f1906170ba6a
jlwebpages <jerlaguna@gmail.com>
2026-02-07 21:38:21 -0800

    Test.

--- scripts.js
+++ scripts.js
@@ -278,7 +278,7 @@ function display_image_with_caption(image_file_name,gallery_name,image_number)
 function display_menu()
 {
    document.getElementById("menu_list").style.width   = "160px";
-   document.getElementById("menu_list").style.height  = 105 + (galleries.length * 40) + "px";
+   document.getElementById("menu_list").style.height  = 110 + (galleries.length * 40) + "px";
    document.getElementById("menu_list").style.padding = "50px 30px 15px 20px";
 
    return true;
