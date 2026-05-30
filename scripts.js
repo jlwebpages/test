17e4d116ef13f192f5b69cafd302936ecf9808b8
jlwebpages <jerlaguna@gmail.com>
2026-05-30 08:07:20 -0700

    Test.

--- scripts.js
+++ scripts.js
@@ -15,13 +15,19 @@ function set_size_of_contact_image_container_and_contact_email_textarea()
 {
    var contact_email_container_element_heights = 0;
    var contact_image                           = null;
+   var contact_image_height                    = 0;
    var contact_image_width                     = 0;
    var scale                                   = 0;
 
 
-   contact_image       = document.querySelector("#contact_image");
-   scale               = contact_image.height / contact_image.naturalHeight;
-   contact_image_width = scale * contact_image.naturalWidth;
+   contact_image        = document.querySelector("#contact_image");
+   scale                = contact_image.height / contact_image.naturalHeight;
+   contact_image_height = scale * contact_image.naturalHeight;
+   contact_image_width  = scale * contact_image.naturalWidth;
+
+    // Set image_container width equal to contact_image width.
+
+   document.getElementById("image_container").width = contact_image_width + "px";
 
    // Calculate contact_email_message text area height.
 
@@ -36,11 +42,7 @@ function set_size_of_contact_image_container_and_contact_email_textarea()
 
    contact_email_container_element_heights += 4;  // Fine tune display.
 
-   document.getElementById("contact_email_message").style.height = "calc(100% - " + contact_email_container_element_heights + "px)";
-
-    // Set image_container width equal to contact_image width.
-
-   document.getElementById("image_container").width = contact_image_width + "px";
+   document.getElementById("contact_email_message").style.height = contact_image_height - contact_email_container_element_heights + "px";
 
    // If running on an iPad in portrait mnode, set contact_email_message width equal to contact_image width.
 
