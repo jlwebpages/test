3ddc87748b4b2115399b800e3de79abb58a3f62d
jlwebpages <jerlaguna@gmail.com>
2026-06-20 15:02:24 -0700

    Test.

--- scripts.js
+++ scripts.js
@@ -883,7 +883,7 @@ function write_footer()
    document.writeln('');
    document.writeln('<div class="copyright">Copyright &copy '+year+' Darlene Laguna Art<br>All Rights Reserved.</div>');
    document.writeln('');
-   document.writeln('<a href="javascript:void(0);"><button id="scroll_button" class="scroll_button" onclick="scroll_to_top();"><span class="scroll_shape"></span></button></a>');
+   document.writeln('<button id="scroll_button" class="scroll_button" onclick="scroll_to_top();"><span class="scroll_shape"></span></button>');
    document.writeln('');
 
    return true;
