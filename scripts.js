1c9a011c235e5351e7591c9d51ff4ad0df88ec1e
jlwebpages <jerlaguna@gmail.com>
2026-07-18 22:40:02 -0700

    Test.

--- scripts.js
+++ scripts.js
@@ -959,3 +959,17 @@ window.addEventListener("scroll", () =>
    }
 }
 );
+
+function toggle()
+{
+   if (document.getElementById("dropdown-content").style.display == "block")
+   {
+      document.getElementById("dropdown-content").style.display = "none";
+   }
+   else
+   {
+      document.getElementById("dropdown-content").style.display = "block";
+   }
+
+   return true;
+}
