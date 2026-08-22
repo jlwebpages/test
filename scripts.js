f71e042884519466f402ee8aa7db8f05b38405a3
jlwebpages <jerlaguna@gmail.com>
2026-08-21 21:04:11 -0700

    Test.

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ var max_image_number = 0;
 // Constant variables.  These values should be set based on the largest image file number from the respective website subfolders.
 
 const gallery_list = [{name: "featured_work",  title: "Featured Work",  min_image_number: 5, max_image_number: 32, new_list: [5,6,7,8]},
-                      {name: "photo_art",      title: "Photo Art",      min_image_number: 6, max_image_number: 16, new_list: [0]},
+                      {name: "photo_art",      title: "Photo Art",      min_image_number: 5, max_image_number: 16, new_list: [5]},
                       {name: "works_on_paper", title: "Works on Paper", min_image_number: 6, max_image_number: 11, new_list: [0]},
                       {name: "sold",           title: "Sold",           min_image_number: 1, max_image_number: 19, new_list: [0]}]
 
